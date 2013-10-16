e62f6b6eb92db3499f4767821cec728693ede106
Roland Walker <walker@pobox.com>
2013-10-16 10:45:45 -0400

    doc/comment update support for 24.3

--- unicode-fonts.el
+++ unicode-fonts.el
@@ -258,7 +258,7 @@
 ;; Compatibility and Requirements
 ;;
 ;;     GNU Emacs version 24.3-devel     : yes, at the time of writing
-;;     GNU Emacs version 24.1 & 24.2    : yes
+;;     GNU Emacs version 24.3           : yes
 ;;     GNU Emacs version 23.3           : yes
 ;;     GNU Emacs version 22.3 and lower : no
 ;;
