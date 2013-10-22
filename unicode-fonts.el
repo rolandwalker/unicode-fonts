66f79ea1ab477060fb1ff42bd6d73b5da7814f80
Roland Walker <walker@pobox.com>
2013-10-22 08:50:03 -0400

    doc/comment works on 24.4 snapshot

--- unicode-fonts.el
+++ unicode-fonts.el
@@ -302,7 +302,7 @@
 ;;
 ;; Compatibility and Requirements
 ;;
-;;     GNU Emacs version 24.4-devel     : no, at the time of writing
+;;     GNU Emacs version 24.4-devel     : yes, at the time of writing
 ;;     GNU Emacs version 24.3           : yes
 ;;     GNU Emacs version 23.3           : yes
 ;;     GNU Emacs version 22.3 and lower : no
