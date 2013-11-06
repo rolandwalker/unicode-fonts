725bb7afb9510fb85ecbba1fc35123f0c7394798
Roland Walker <walker@pobox.com>
2013-11-06 10:06:42 -0500

    update/stdize copyrights

--- unicode-fonts.el
+++ unicode-fonts.el
@@ -1,6 +1,6 @@
 ;;; unicode-fonts.el --- Configure Unicode fonts
 ;;
-;; Copyright (c) 2012 Roland Walker
+;; Copyright (c) 2012-13 Roland Walker
 ;;
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/unicode-fonts
