d559653af93ce101bf52c7955d99ea175f017292
Roland Walker <walker@pobox.com>
2015-08-12 12:17:14 -0400

    update release compat table

--- README.markdown
+++ README.markdown
@@ -431,7 +431,9 @@ of blocks and glyphs.
 
 ## Compatibility and Requirements
 
-	GNU Emacs version 24.4-devel     : yes, at the time of writing
+	GNU Emacs version 25.1-devel     : not tested
+	GNU Emacs version 24.5           : not tested
+	GNU Emacs version 24.4           : yes
 	GNU Emacs version 24.3           : yes
 	GNU Emacs version 23.3           : yes
 	GNU Emacs version 22.3 and lower : no
--- unicode-fonts.el
+++ unicode-fonts.el
@@ -387,7 +387,9 @@
 ;;
 ;; Compatibility and Requirements
 ;;
-;;     GNU Emacs version 24.4-devel     : yes, at the time of writing
+;;     GNU Emacs version 25.1-devel     : not tested
+;;     GNU Emacs version 24.5           : not tested
+;;     GNU Emacs version 24.4           : yes
 ;;     GNU Emacs version 24.3           : yes
 ;;     GNU Emacs version 23.3           : yes
 ;;     GNU Emacs version 22.3 and lower : no
